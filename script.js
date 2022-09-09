bd74f891d3387810b109e3658da843171ca522a8
Russell James <russelljames202@gmail.com>
2022-09-08 20:26:45 -0400

    Add slider to change grid size
    Added a slider to change grid size to a max value of 100.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * Calls necessary functions
  */
 function main(){
-    createGrid();
+    createGrid(16);
     addEventListeners();
 }
 
@@ -31,17 +31,28 @@ function createGrid(size){
  */
 function addEventListeners(){
     //For each box in grid
-    boxes = document.querySelectorAll('.box');
+    const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => box.addEventListener('mouseover', 
                                                 () => box.style.backgroundColor = 'black'));
 
     //Clear button
-    clearButton = document.querySelector('.clear-button');
+    const clearButton = document.querySelector('.clear-button');
     clearButton.addEventListener('click', clearGrid);
 
     //Erase button
-    eraseButton = document.querySelector('.erase-button');
-    eraseButton.addEventListener('click', eraseGrid)
+    const eraseButton = document.querySelector('.erase-button');
+    eraseButton.addEventListener('click', eraseGrid);
+
+    //Slider
+    const slider = document.querySelector('#grid-size');
+    const sliderValue = document.querySelector('.slider-value');
+
+    slider.addEventListener('input', () => sliderValue.innerText = `${slider.value} x ${slider.value}`);
+
+    //Apply button
+    const applyButton = document.querySelector('.apply-button');
+
+    applyButton.addEventListener('click', changeGridSize);
 
 }
 
@@ -49,14 +60,18 @@ function addEventListeners(){
  * Callback function for clear button
  */
 function clearGrid(){
+    const boxes = document.querySelectorAll('.box');
+
     //Changes all boxes' background back to original color
-    boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => box.style.backgroundColor = 'white');
 }
 
 /*
-* TODO: add slider to choose grid size
-        do 'extra credit' portion
+* TODO: let user know they are in erase mode
+        separate each event listener into its own function
+        have addEventListeners call each function
+        add color picker button
+        do 'extra credit' portion, rainbow mode button
         add more css to website
         **commit along the way
 */
@@ -64,7 +79,7 @@ function clearGrid(){
 function eraseGrid(){
     
     //Reference to box elements in grid
-    boxes = document.querySelectorAll('.box');
+    const boxes = document.querySelectorAll('.box');
     
     //Lets user alternate between drawing and erasing
     if(this.innerText === 'Erase'){    
@@ -78,6 +93,32 @@ function eraseGrid(){
     }
 
 }
+
+function changeGridSize(){
+    //Get slider value and rows reference
+    const sliderValue = document.querySelector('#grid-size').value;
+    const grid = document.querySelector('.grid');
+
+    //Set erase button back to original text 
+    //Draw mode will be on by default after changing grid size
+    const eraseButton = document.querySelector('.erase-button');
+    eraseButton.innerText = 'Erase';
+
+    //Remove all elements from grid
+    while(grid.firstChild){
+        console.log(grid.firstChild);
+        grid.removeChild(grid.firstChild);
+    }
+
+    //Create new grid
+    createGrid(sliderValue);
+
+
+    //TODO: just add grid even listener
+    addEventListeners();
+}
+
+    
 
 
 main();

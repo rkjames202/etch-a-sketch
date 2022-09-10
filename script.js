4b30a4d0f94da569691040c001e45556ab83bbff
Russell James <russelljames202@gmail.com>
2022-09-10 01:12:31 -0400

    Refactor and add functions for new features
    Added two modes to the existing feature, erase. As well as the new
    features, rainbow and change color. Helper functions for these three
    features were added.
    
    They include, checkEraseMode(), getEraseColor, turnOffEraseMode() for
    erase mode. changeColor(), getcolorPickerValue() for color picker.
    activateRainbow(), getRainbowcolors() and checkRainbowmode() for
    rainbow mode.
    
    Changed logic of addBoxEventListeners(). Now checks for erase mode and
    rainbow mode and uses color picker value as default color if no
    parameter is given.
    
    Also changed logic of eraseGrid() to only check for text in erase
    button.
    
    clearGrid() now turns off erase mode.
    
    Lastly, changeGridSize() now displays error message when user tries to
    apply new grid size without changing slider value.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 /**
- * Calls necessary functions
+ * Calls necessary functions when webpage is loaded 
  */
 function main(){
     createGrid(16);
     addEventListeners();
 }
 
+/**
+ * Creates a 'size' x 'size' grid row by row
+ * 
+ * @param size - number of boxes inside of grid 
+ */
 function createGrid(size){
-    //Reference to container element
+    //Reference to grid/pad 
     const grid = document.querySelector(".grid");
 
     //Create 'size' amount of rows
@@ -23,18 +28,59 @@ function createGrid(size){
             row.appendChild(box);
         }
     }
-
 }
 
 /**
  * Adds event listeners to elements 
  */
 function addEventListeners(){
-    //For each box in grid
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach((box) => box.addEventListener('mouseover', 
-                                                () => box.style.backgroundColor = 'black'));
+    addBoxEventListeners();
+    addButtonEventListeners();
+    addSliderEventListener();
+}
 
+
+/**
+ * Adds event listeners to each box inside of the grid
+ * depending on what 'mode' is active
+ * 
+ * @param color = color of boxes inside grid 
+ * @returns nothing
+ */
+function addBoxEventListeners(color){
+    //Reference to boxes
+    const boxes = document.querySelectorAll('.box');
+
+    //The two following if statements check for erase and rainbow mode
+    //Erase mode has higher precedence than rainbow
+    if(checkEraseMode()){
+        boxes.forEach((box) => box.addEventListener('mouseover', getEraseColor));
+        return; //Will exit function once event listener is added
+    } else {
+        boxes.forEach((box) => box.removeEventListener('mouseover', getEraseColor));
+    }
+
+    if(checkRainbowMode()){
+        boxes.forEach((box) => box.addEventListener('mouseover', getRainbowColors));
+        return;
+    }else{
+        boxes.forEach((box) => box.removeEventListener('mouseover', getRainbowColors));
+    }
+
+    //If no mode is active, default to parameter or color picker value
+    if(arguments[0]){
+        boxes.forEach((box) => box.addEventListener('mouseover', () => box.style.backgroundColor = color));
+
+    }else{
+        boxes.forEach((box) => box.addEventListener('mouseover', getColorPickerValue));
+        
+    } 
+}
+
+/**
+ * Adds event listeners to buttons
+ */
+function addButtonEventListeners(){
     //Clear button
     const clearButton = document.querySelector('.clear-button');
     clearButton.addEventListener('click', clearGrid);
@@ -43,82 +89,207 @@ function addEventListeners(){
     const eraseButton = document.querySelector('.erase-button');
     eraseButton.addEventListener('click', eraseGrid);
 
-    //Slider
-    const slider = document.querySelector('#grid-size');
-    const sliderValue = document.querySelector('.slider-value');
-
-    slider.addEventListener('input', () => sliderValue.innerText = `${slider.value} x ${slider.value}`);
-
     //Apply button
     const applyButton = document.querySelector('.apply-button');
-
     applyButton.addEventListener('click', changeGridSize);
 
+    //Color Picker
+    const colorPicker = document.querySelector('#color-picker');
+    colorPicker.addEventListener('input', changeColor);
+
+    //RainbowButton
+    const rainbowButton = document.querySelector('.rainbow-button');
+    rainbowButton.addEventListener('click', activateRainbow);
 }
 
 /**
- * Callback function for clear button
+ * Adds event listener to slider, 
+ * displaying grid size on webpage 
+ */
+function addSliderEventListener(){
+        //Slider and current value in slider
+        const slider = document.querySelector('#grid-size');
+        const sliderValue = document.querySelector('.slider-value');
+
+        //Display current slider value on change
+        slider.addEventListener('input', () => sliderValue.innerText = `${slider.value} x ${slider.value}`);
+}
+
+/**
+ * **Callback function
+ * Clears grid
  */
 function clearGrid(){
+    //Each box in grid
     const boxes = document.querySelectorAll('.box');
 
     //Changes all boxes' background back to original color
     boxes.forEach((box) => box.style.backgroundColor = 'white');
+
+    //Turn erase mode off
+    turnOffEraseMode();
+
+    //Re-add box listeners
+    addBoxEventListeners();
 }
 
-/*
-* TODO: let user know they are in erase mode
-        separate each event listener into its own function
-        have addEventListeners call each function
-        add color picker button
-        do 'extra credit' portion, rainbow mode button
-        add more css to website
-        **commit along the way
-*/
 
-function eraseGrid(){
-    
-    //Reference to box elements in grid
-    const boxes = document.querySelectorAll('.box');
+/**
+ * **Callback function
+ * Sets grid to 'erase' mode
+ */
+function eraseGrid(){  
     
     //Lets user alternate between drawing and erasing
-    if(this.innerText === 'Erase'){    
-        this.innerText = 'Draw'; 
-        boxes.forEach((box) => box.addEventListener('mouseover', 
-                                                    () => box.style.backgroundColor = 'white'));
-    } else if(this.innerText === 'Draw'){
-        this.innerText = 'Erase';
-        boxes.forEach((box) => box.addEventListener('mouseover', 
-                                                    () => box.style.backgroundColor = 'black'));
+    if(this.classList.contains('erasing')){  
+        this.innerText = 'Erase'; 
+        this.classList.remove('erasing');
+    } else {
+        this.innerText = 'Erasing'; 
+        this.classList.add('erasing');   
     }
+
+    //Re-add box listeners to detect erase mode
+    addBoxEventListeners();
 
 }
 
-function changeGridSize(){
-    //Get slider value and rows reference
-    const sliderValue = document.querySelector('#grid-size').value;
-    const grid = document.querySelector('.grid');
+/**
+ * Gets the color for applied to boxes in
+ * erase mode
+ */
+function getEraseColor(){
+    this.style.backgroundColor = 'white';
+}
 
-    //Set erase button back to original text 
-    //Draw mode will be on by default after changing grid size
+/**
+ * Turns off erase mode 
+ * Used in any function that isn't eraseGrid();
+ */
+function turnOffEraseMode(){
     const eraseButton = document.querySelector('.erase-button');
     eraseButton.innerText = 'Erase';
+    eraseButton.classList.remove('erasing');
+}
+
+/**
+ * Checks if erase mode is active
+ * 
+ * @returns if erase mode is active
+ */
+ function checkEraseMode(){
+    const eraseButton = document.querySelector('.erase-button');
+
+    if (eraseButton.innerText === 'Erasing') return true;
+
+    return false;
+}
+
+/**
+ * Changes the size of grid
+ * 
+ * @returns nothing
+ */
+function changeGridSize(){
+    //Get slider value and grid reference
+    let sliderValue = document.querySelector('#grid-size').value;
+    const grid = document.querySelector('.grid'); 
+
+    //Get current size of rows
+    const row = document.querySelector('.row');
+    let rowSize = row.querySelectorAll('.box').length;
+    
+    //Error message below apply button
+    const errorMessage = document.querySelector('.grid-error-message');
+
+    //If the slider hasn't moved, display error message and exit function 
+    if(rowSize == sliderValue){
+        errorMessage.style.visibility = 'visible';
+        return;
+    } else{
+        errorMessage.style.visibility = 'hidden'; 
+    }
+
+    //Turn erase mode off
+    turnOffEraseMode();
 
     //Remove all elements from grid
     while(grid.firstChild){
-        console.log(grid.firstChild);
         grid.removeChild(grid.firstChild);
     }
 
     //Create new grid
     createGrid(sliderValue);
 
+    //Re-add box listeners to new grid
+    addBoxEventListeners();
 
-    //TODO: just add grid even listener
-    addEventListeners();
 }
 
+/**
+ * Change the drawing color of grid
+ */
+function changeColor(){
+    //Re-add box listeners to get color in color picker
+    addBoxEventListeners();
+    //Changes background of input element to make the selected color
+    this.style.backgroundColor = `${this.value}`;
+}
+
+
+/**
+ * Gets value of color picker
+ */
+function getColorPickerValue(){
+    let color = document.querySelector('#color-picker').value;
+    this.style.backgroundColor = color;
+}
+
+/**
+ * Activates/deactivates 'rainbow mode
+ */
+function activateRainbow(){  
+    //If 'rainbow' mode is deactivate it, if not activate it
+    if(this.classList.contains('rainbow-button-active')){       
+        
+        this.classList.remove('rainbow-button-active');           
+        addBoxEventListeners(); 
     
+    } else {
+        
+        this.classList.add('rainbow-button-active');
+        addBoxEventListeners();
+    
+    }
+}
+
+/**
+ * Get random rgb values each time event
+ * is fired in 'rainbow' mode
+ */
+function getRainbowColors(){
+            this.style.backgroundColor = `rgb(${Math.floor(Math.random()*255) +1},
+                                            ${Math.floor(Math.random()*255) +1},
+                                            ${Math.floor(Math.random()*255) +1})`
+}
+
+/**
+ * Checks if rainbow mode is active
+ * 
+ * @returns if rainbow mode is active 
+ */
+function checkRainbowMode(){
+    
+    const rainbowButton = document.querySelector('.rainbow-button');
+
+    if(rainbowButton.classList.contains('rainbow-button-active')) return true;
+
+    //If not return false
+    return false;
+
+}
+
+
 
 
 main();
